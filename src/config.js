ae95f28113d536c7d894f45b7760f522c94caaa3
Max Martyn <max.martyn@pragmaticdlt.com>
2018-06-12 11:27:39 +0300

    Deleted unused API utils and configs

--- src/config.js
+++ src/config.js
@@ -2,11 +2,6 @@
 
 export default {
   displayDevTools: process.env.NODE_ENV !== 'production',
-  API:
-    process.env.NODE_ENV === 'production'
-      ? 'http://xxx.com'
-      : 'http://localhost:3933',
-  authToken: null,
   web3: {
     provider: "http://localhost:7545"
   }
